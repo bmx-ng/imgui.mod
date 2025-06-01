229bea643a42e9b6ec150265a0dba5a9d18bd0af
Brucey <woollybah@gmail.com>
2025-06-01 22:41:58 +0100

    Updates event callback for user data
    Updates the event callback to include user data and priority, allowing for more flexible event handling.
    
    This change enables passing additional information to the callback function and setting the order in which callbacks are executed.

--- imguisdl2.mod/glue.c
+++ imguisdl2.mod/glue.c
@@ -1,10 +1,10 @@
 
 #include "dcimgui_impl_sdl2.h"
 
-typedef void (*EventCallback)(SDL_Event *event, int *ignoreKeyboard, int *ignoreMouse);
-extern void bmx_SDL_SetEventCallback(EventCallback callback);
+typedef void (*EventCallback)(void *userdata, SDL_Event *event, int *ignoreKeyboard, int *ignoreMouse);
+extern void bmx_SDL_AddEventCallback(EventCallback callback, void *userdata, int priority);
 
-static void bmx_imguisdl2_eventCallback(SDL_Event *event, int *ignoreKeyboard, int *ignoreMouse) {
+static void bmx_imguisdl2_eventCallback(void *userdata, SDL_Event *event, int *ignoreKeyboard, int *ignoreMouse) {
     cImGui_ImplSDL2_ProcessEvent(event);
     ImGuiIO * io = ImGui_GetIO();
     *ignoreKeyboard = io->WantCaptureKeyboard;
@@ -12,5 +12,5 @@ static void bmx_imguisdl2_eventCallback(SDL_Event *event, int *ignoreKeyboard, i
 }
 
 void bmx_imguisdl2_init() {
-    bmx_SDL_SetEventCallback(bmx_imguisdl2_eventCallback);
+    bmx_SDL_AddEventCallback(bmx_imguisdl2_eventCallback, NULL, 9999);
 }
